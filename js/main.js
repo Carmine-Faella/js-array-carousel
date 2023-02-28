0f83752309262aabab9849371c8e2519633a7af5
Carmine Faella <carminefaella5@gmail.com>
2023-02-28 21:28:05 +0100

    Finita anche implementazione secondo bonus

--- js/main.js
+++ js/main.js
@@ -8,12 +8,22 @@ const array = [
 
 const imageDom = document.querySelector('.images');
 
+const imageContainerDom = document.querySelector('.image-container');
+
 let columnContent = "";
+
+let imageContent = ""; 
 
 for (let i = 0; i < array.length; i++) {
     const card = `<div class="card-image">
                         <img class="img" src="${array[i]}" />
                   </div>`;
+
+    const newImage = `<div class="image-back">
+                            <img class="img-back" src="${array[i]}" />
+                       </div>`;
+
+    imageContent += newImage;
 
     columnContent += card;       
 }
@@ -21,12 +31,15 @@ for (let i = 0; i < array.length; i++) {
 console.log(array)
 
 imageDom.innerHTML = columnContent;
+imageContainerDom.innerHTML = imageContent;
 
 const cardContainer = document.getElementsByClassName('card-image');
+const imageBackDom = document.getElementsByClassName('image-back');
 
 let activeImage = 0;
 
 cardContainer[activeImage].classList.add('d-block'); 
+imageBackDom[activeImage].classList.add('current');
 
 const nextDom = document.querySelector('#next');
 const prevDom = document.querySelector('#prev');
@@ -36,13 +49,17 @@ nextDom.addEventListener('click',
         if (activeImage < cardContainer.length - 1) {
 
             cardContainer[activeImage].classList.remove('d-block');
+            imageBackDom[activeImage].classList.remove('current');
             activeImage++;
             cardContainer[activeImage].classList.add('d-block');
+            imageBackDom[activeImage].classList.add('current');
 
         }else{
             cardContainer[activeImage].classList.remove('d-block');
+            imageBackDom[activeImage].classList.remove('current');
             activeImage = 0;
             cardContainer[activeImage].classList.add('d-block');
+            imageBackDom[activeImage].classList.add('current');
         }
     }
 );
@@ -52,13 +69,17 @@ prevDom.addEventListener('click',
         if (activeImage > 0) {
 
             cardContainer[activeImage].classList.remove('d-block');
+            imageBackDom[activeImage].classList.remove('current');
             activeImage--;
             cardContainer[activeImage].classList.add('d-block');
+            imageBackDom[activeImage].classList.add('current');
 
         }else{
             cardContainer[activeImage].classList.remove('d-block');
+            imageBackDom[activeImage].classList.remove('current');
             activeImage = [4];
             cardContainer[activeImage].classList.add('d-block');
-        }
+            imageBackDom[activeImage].classList.add('current');
+        }   
     }
 );
